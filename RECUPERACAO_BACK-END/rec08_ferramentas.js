3c8662207ee8fe40709ebe4d44f70de7d529cdee
Pablo_HRSpx <deconhecimentoxx@gmail.com>
2026-09-23 11:18:48 -0300

    RECUPERACAO

--- RECUPERACAO_BACK-END/rec08_ferramentas.js
+++ RECUPERACAO_BACK-END/rec08_ferramentas.js
@@ -4,4 +4,14 @@ const ferramentas = [];
 
 for (let i = 0; i <= 3; i ++) {
     const nome = entrada.question(`Digite o nome da ferramenta ${i+1}: `);
+    const quantidade = entrada.questionInt("Digite a quantidade de ferramentas: ");
+    const minimo = entrada.questionInt("Digite o minimo de ferramentas: ");
+    ferramentas.push (nome,quantidade,minimo)
+}
+
+if (quantidade < minimo && ferramentas.length) {
+    console.log(`Nome da ferramenta: ${ferramentas[i]}`);
+    console.log(`Quantidade de ferramenta: ${ferramentas[i]}`);
+    console.log(`Minimo de ferramentas: ${ferramentas[i]}`);
+    console.log("REPOR")
 }
